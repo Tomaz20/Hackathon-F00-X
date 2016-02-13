759937ca29e646860cb1dd0ff810788f0cef6807
Tomas Ferreira <ttomaz.200@gmail.com>
2016-02-13 02:42:31 +0000

    coisital

--- pages/sintomas/scripts/lista_sintomas.php
+++ pages/sintomas/scripts/lista_sintomas.php
@@ -2,13 +2,13 @@
 
 	$dbh = new PDO('mysql:host=localhost;dbname=hackathon', 'root', 'root');
 	
-	$qstring = "SELECT id_sintoma, designacao FROM Sintoma";
+	$qstring = "SELECT id_sintoma, nome FROM Sintoma";
 	$resultado = $dbh->query($qstring);
 	
 	$res = "";
 	
 	while($sintoma = $resultado->fetch()){
-		$res .= "<option value='".$sintoma['id_sintoma']."'>".$sintoma['designacao']."</option>";
+		$res .= "<option value='".$sintoma['id_sintoma']."'>".$sintoma['nome']."</option>";
 	}	
 	
 	echo $res;
--- pages/sintomas/scripts/pesquisar_sintomas.js
+++ pages/sintomas/scripts/pesquisar_sintomas.js
@@ -1,7 +1,45 @@
 $(function(){
+	var nsintomas=0;
+
+
 	$("#lista_sintomas").load("scripts/lista_sintomas.php");
 
-	$("#addsintoma").onclick=function(){
-		$("#sintomas").append("<div>blablabla</div>");
-	}
+	$("#addsintoma").click(function(){
+		
+		nsintomas++;
+
+        var novoSintoma = document.createElement('div');
+           
+        novoSintoma.setAttribute("id",'s' + nsintomas);
+		
+		var divSint = document.createElement('div');
+		
+		divSint.setAttribute("class", "form-group");
+		
+		var labelSint = document.createElement('label');
+		$(labelSint).append("Sintoma");
+		
+		var selectSint = document.createElement('select');
+		
+		selectSint.setAttribute("class", "form-control");
+		selectSint.setAttribute("name", "sintoma[" + nsintomas + "]");
+		
+		var options = $("#lista_sintomas > option").clone();
+		
+		$(selectSint).append(options);
+		
+		divSint.appendChild(labelSint);
+		divSint.appendChild(selectSint);
+		
+		novoSintoma.appendChild(divSint);
+       
+        $(novoSintoma).appendTo("#sintomas");
+	});
+
+	$("#remsintoma").click(function(){	
+		if(nsintomas>0){
+			$("#s"+nsintomas).remove();
+			nsintomas--;
+		}
+	}); 
 }); 
